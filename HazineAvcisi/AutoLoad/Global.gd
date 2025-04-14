74d303539865ff3d7603bca95dc811b2f23ccafc
ilyas <erturunilyas120@gmail.com>
2025-04-15 00:18:26 +0300

    Global script oluşturma
    oyuncunun skor verisini ekleme

---
+++ HazineAvcisi/AutoLoad/Global.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var playerData = {
+	"skor" :0
+}
+
+
+var setGetKullanimi = false setget setGetKullanimiSet, setGetKullanimiGet
+	
+func setGetKullanimiSet(value):
+	setGetKullanimi = value
+	
+func setGetKullanimiGet():
+	return setGetKullanimi
