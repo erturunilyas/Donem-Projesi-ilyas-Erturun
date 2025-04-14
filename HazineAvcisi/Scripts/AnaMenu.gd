6a17c0f2e87bda4246d4bd40db66467601b58d86
ilyas <erturunilyas120@gmail.com>
2025-04-14 23:29:12 +0300

    Anamenü işlemleri
    butonlara işlevsellik ekleme

--- HazineAvcisi/Scripts/AnaMenu.gd
+++ HazineAvcisi/Scripts/AnaMenu.gd
@@ -1,7 +1,20 @@
 extends Control
 
-
-
 func _ready():
+	
+	
+	get_tree().paused = false
 	pass 
 
+
+
+
+func _on_btn_play_pressed():
+	get_tree().change_scene("res://Sahneler/Level1.tscn")
+	pass
+
+
+func _on_btn_exit_pressed():
+	
+	get_tree().quit()
+	pass
