704796dcc3c95753c2315e36c2e2b31fb3467d06
ilyas <erturunilyas120@gmail.com>
2025-04-14 23:53:14 +0300

    girdi değerleri
    başlşangıç değerleri

--- HazineAvcisi/Scripts/Player.gd
+++ HazineAvcisi/Scripts/Player.gd
@@ -1,16 +1,14 @@
 extends KinematicBody2D
 
+const HIZ = 200
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+const JUMP_POWER = 700
+const GRAVITY = 40
+var hizVektoru : Vector2
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	hizVektoru = Vector2(0, 0) 
+	
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
