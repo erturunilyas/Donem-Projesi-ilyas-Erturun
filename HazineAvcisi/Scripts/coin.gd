0b14480505412146653a055488cfcfa991007734
ilyas <erturunilyas120@gmail.com>
2025-04-15 22:41:28 +0300

    coin
    coin için animasyon çalıştırma ve skor değeri verme

--- HazineAvcisi/Scripts/coin.gd
+++ HazineAvcisi/Scripts/coin.gd
@@ -1,6 +1,17 @@
 extends Area2D
-
+var skorDegeri = 1
 
 
 func _ready():
-	pass 
+	$AnimatedSprite.play("coin_move")
+	pass
+
+
+
+func _on_coin_body_entered(body):
+	if body.name == "Player":
+		Global.playerData.skor += skorDegeri
+		
+		# mevcut script çalıştığı düğümü sahneden siler
+		queue_free()
+	pass
